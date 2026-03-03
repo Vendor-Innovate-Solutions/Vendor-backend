5c2e46db6aac5abbc5678ac169c8ca02cd75d69a
joshuakarthik2005 <joshuakarthik2005@gmail.com>
2026-03-03 23:17:25 +0530

    fix: tally_import admin fields to match models

--- apps/system/tally_import/admin.py
+++ apps/system/tally_import/admin.py
@@ -7,41 +7,25 @@ from .models import TallyImportJob, TallyFieldMapping, TallyImportRecord, TallyM
 
 @admin.register(TallyImportJob)
 class TallyImportJobAdmin(admin.ModelAdmin):
-    list_display = ['id', 'company', 'file_name', 'status', 'total_records', 
-                   'imported_records', 'failed_records', 'created_at']
+    list_display = ['id', 'company', 'original_filename', 'status', 'total_records',
+                   'processed_records', 'failed_records', 'created_at']
     list_filter = ['status', 'created_at']
-    search_fields = ['file_name', 'tally_company_name']
+    search_fields = ['original_filename', 'tally_company_name']
     readonly_fields = ['id', 'created_at', 'started_at', 'completed_at']
-    
-    fieldsets = (
-        ('Job Info', {
-            'fields': ('id', 'company', 'file_name', 'file_size', 'status')
-        }),
-        ('Tally Info', {
-            'fields': ('tally_company_name', 'tally_version', 'data_types')
-        }),
-        ('Progress', {
-            'fields': ('total_records', 'processed_records', 'imported_records', 
-                      'failed_records', 'skipped_records', 'error_message')
-        }),
-        ('Timestamps', {
-            'fields': ('created_at', 'started_at', 'completed_at', 'created_by')
-        }),
-    )
 
 
 @admin.register(TallyFieldMapping)
 class TallyFieldMappingAdmin(admin.ModelAdmin):
-    list_display = ['import_job', 'data_type', 'tally_field', 'system_field', 'is_active']
-    list_filter = ['data_type', 'is_active']
-    search_fields = ['tally_field', 'system_field']
+    list_display = ['id', 'name', 'data_type', 'is_default']
+    list_filter = ['data_type', 'is_default']
+    search_fields = ['name']
 
 
 @admin.register(TallyImportRecord)
 class TallyImportRecordAdmin(admin.ModelAdmin):
-    list_display = ['import_job', 'data_type', 'tally_name', 'status', 'system_id', 'created_at']
+    list_display = ['import_job', 'data_type', 'tally_name', 'status', 'created_at']
     list_filter = ['status', 'data_type']
-    search_fields = ['tally_name', 'tally_guid', 'system_id']
+    search_fields = ['tally_name', 'tally_guid']
     readonly_fields = ['id', 'created_at']
 
 
